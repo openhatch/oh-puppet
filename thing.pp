b988f2475fe6342a2662ccb7e021e9c95415a1c9
Asheesh Laroia <asheesh@asheesh.org>
2011-06-12 00:25:12 +0000

    trying

--- thing.pp
+++ thing.pp
@@ -1,2 +1,33 @@
+class openhatch_code {
+  user { 'deploy':
+    comment => "Mister Deploy",
+    home => "/home/deploy",
+    shell => "/bin/bash",
+    uid => '50000', # Let's give it a constant UID
+  }
+
+  group { 'deploy':
+    gid => '50000',
+    require => User['deploy'],
+  }
+
+  package { 'git':
+    ensure => 'installed',
+  }
+
+  # We do not use the vcsrepo type because
+  # it does not support setting the user
+  # who will own the resulting git clone.
+  exec { "/usr/bin/git clone git://gitorious.org/openhatch/oh-mainline.git":
+    user => 'deploy',
+    group => 'deploy',
+    cwd => '/home/deploy/',
+    logoutput => "on_failure",
+    unless => '/usr/bin/test -d /home/deploy/oh-mainline'
+  }
+
+}
+
 node default {
+  openhatch_code
 }
